1848b89c3fde2ba2e06d86e2dafa2e03885b8945
emrsmsrli <emrsmsrli1@gmail.com>
2020-10-02 14:48:42 +0300

    add entt::delegate and a basic event class on top of it

---
+++ gba/include/gba/core/event/event.h
@@ -0,0 +1,30 @@
+#ifndef GAMEBOIADVANCE_EVENT_H
+#define GAMEBOIADVANCE_EVENT_H
+
+#include <vector>
+
+#include <gba/core/event/delegate.h>
+
+namespace gba {
+
+template<typename... Args>
+class event {
+    std::vector<delegate<void(Args...)>> delegates;
+
+public:
+    event() = default;
+
+    void add_delegate(delegate<void(Args...)> d) { delegates.push_back(d); }
+    void clear_delegates() noexcept { delegates.clear(); }
+
+    template<typename... TArgs>
+    void operator()(TArgs&&... args) const {
+        for(auto& d : delegates) {
+            d(std::forward<TArgs>(args)...);
+        }
+    }
+};
+
+} // namespace gba
+
+#endif //GAMEBOIADVANCE_EVENT_H
