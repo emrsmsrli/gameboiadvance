ff7a560b4b439e17adfb31f4313704c4f665730f
emrsmsrli <emrsmsrli1@gmail.com>
2020-10-18 01:07:28 +0300

    add logging macros and use them in filesystem.cpp

--- gba/include/gba/helper/macros.h
+++ gba/include/gba/helper/macros.h
@@ -3,27 +3,34 @@
 
 #include <spdlog/spdlog.h>
 
+#define LOG_CRITICAL(fmt, ...) SPDLOG_CRITICAL(fmt, __VA_ARGS__) /*NOLINT*/
+#define LOG_ERROR(fmt, ...) SPDLOG_ERROR(fmt, __VA_ARGS__) /*NOLINT*/
+#define LOG_WARN(fmt, ...) SPDLOG_WARN(fmt, __VA_ARGS__) /*NOLINT*/
+#define LOG_INFO(fmt, ...) SPDLOG_INFO(fmt, __VA_ARGS__) /*NOLINT*/
+#define LOG_DEBUG(fmt, ...) SPDLOG_DEBUG(fmt, __VA_ARGS__) /*NOLINT*/
+#define LOG_TRACE(fmt, ...) SPDLOG_TRACE(fmt, __VA_ARGS__) /*NOLINT*/
+
 #if defined(_MSC_VER)
   #define FORCEINLINE __forceinline
-  #define UNREACHABLE()                                                                     \
-    do {                                                                                    \
-        SPDLOG_CRITICAL("unreachable code hit at {}:{}", __FILE__, __LINE__); /*NOLINT*/    \
-        __assume(0);                                                                        \
+  #define UNREACHABLE()                                                         \
+    do {                                                                        \
+        LOG_CRITICAL("unreachable code hit at {}:{}", __FILE__, __LINE__);      \
+        __assume(0);                                                            \
     } while(0)
 #elif defined(__GNUC__) && __GNUC__ > 3
   // Clang also defines __GNUC__ (as 4)
   #define FORCEINLINE inline __attribute__ ((__always_inline__))
-  #define UNREACHABLE()                                                                     \
-    do {                                                                                    \
-        SPDLOG_CRITICAL("unreachable code hit at {}:{}", __FILE__, __LINE__); /*NOLINT*/    \
-        __builtin_unreachable();                                                            \
+  #define UNREACHABLE()                                                         \
+    do {                                                                        \
+        LOG_CRITICAL("unreachable code hit at {}:{}", __FILE__, __LINE__);      \
+        __builtin_unreachable();                                                \
     } while(0)
 #else
   #define FORCEINLINE inline
-  #define UNREACHABLE()                                                                     \
-    do {                                                                                    \
-        SPDLOG_CRITICAL("unreachable code hit at {}:{}", __FILE__, __LINE__); /*NOLINT*/    \
-        std::terminate();                                                                   \
+  #define UNREACHABLE()                                                         \
+    do {                                                                        \
+        LOG_CRITICAL("unreachable code hit at {}:{}", __FILE__, __LINE__);      \
+        std::terminate();                                                       \
     } while(0)
 #endif
 
--- gba/src/helper/filesystem.cpp
+++ gba/src/helper/filesystem.cpp
@@ -10,6 +10,7 @@ vector<u8> read_file(const fs::path& path)
     std::ifstream stream{path, std::ios::binary | std::ios::in};
     stream.unsetf(std::ios::skipws);
     if(!stream.is_open()) {
+        LOG_CRITICAL("input file stream could not be opened: {}", path.string());
         std::terminate();
     }
 
@@ -21,6 +22,7 @@ vector<u8> read_file(const fs::path& path)
         std::copy_n(buffer.begin(), stream.gcount(), std::back_inserter(bytes.underlying_data()));
     }
 
+    LOG_TRACE("read {} bytes from {}", bytes.size(), path.string());
     return bytes;
 }
 
@@ -29,10 +31,12 @@ void write_file(const fs::path& path, const vector<u8>& data)
     std::ofstream stream{path, std::ios::binary | std::ios::out};
     stream.unsetf(std::ios::skipws);
     if(!stream.is_open()) {
+        LOG_CRITICAL("output file stream could not be opened: {}", path.string());
         std::terminate();
     }
 
     stream.write(reinterpret_cast<const char*>(data.data()), data.size().get()); // NOLINT
+    LOG_TRACE("wrote {} bytes to {}", data.size(), path.string());
 }
 
 } // namespace gba
