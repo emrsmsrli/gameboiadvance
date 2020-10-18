e55d611fefc581b9540f28ec6ef0aafbd7b419b2
emrsmsrli <emrsmsrli1@gmail.com>
2020-10-18 17:23:41 +0300

    add gba::bit ops

--- gba/include/gba/core/math.h
+++ gba/include/gba/core/math.h
@@ -1,20 +1,45 @@
 #ifndef GAMEBOIADVANCE_MATH_H
 #define GAMEBOIADVANCE_MATH_H
 
-namespace gba::math {
+#include <gba/core/integer.h>
+#include <gba/helper/macros.h>
+
+namespace gba {
+
+namespace math {
 
 template<typename T>
-T min(T f, T s) noexcept
-{
-    return f < s ? f : s;
-}
+[[nodiscard]] FORCEINLINE constexpr T min(T f, T s) noexcept { return f < s ? f : s; }
 
 template<typename T>
-T max(T f, T s) noexcept
+[[nodiscard]] FORCEINLINE constexpr T max(T f, T s) noexcept { return f > s ? f : s; }
+
+} // namespace math
+
+namespace bit {
+
+[[nodiscard]] FORCEINLINE constexpr u32 from_bool(const bool b) noexcept { return static_cast<uint32_t>(b); }
+
+template<typename T>
+[[nodiscard]] FORCEINLINE constexpr u32 bit(const T b) noexcept { return 1_u32 << b; }
+
+template<typename T, typename TBit>
+[[nodiscard]] FORCEINLINE constexpr bool test(const T t, const TBit b) noexcept { return (t & bit(b)) == 1u; }
+
+template<typename T, typename TBit>
+[[nodiscard]] FORCEINLINE constexpr bool set(const T t, const TBit b) noexcept { return t | bit(b); }
+
+template<typename T, typename TBit>
+[[nodiscard]] FORCEINLINE constexpr bool clear(const T t, const TBit b) noexcept { return t & ~bit(b); }
+
+template<typename T, typename TBit>
+[[nodiscard]] FORCEINLINE constexpr u32 extract(const T value, const TBit bit) noexcept
 {
-    return f > s ? f : s;
+    return bit::from_bool(bit::test(value, bit));
 }
 
-} // namespace gba::math
+} // namespace bit
+
+} // namespace gba
 
 #endif //GAMEBOIADVANCE_MATH_H
