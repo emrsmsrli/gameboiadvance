1c3cc2db6ef3b6b3c345f69bc4ece96b6791251e
emrsmsrli <emrsmsrli1@gmail.com>
2020-10-21 18:32:18 +0300

    add debugger helper integer fmt

---
+++ gba_debugger/include/gba_debugger/fmt_integer.h
@@ -0,0 +1,15 @@
+#ifndef GAMEBOIADVANCE_FMT_INTEGER_H
+#define GAMEBOIADVANCE_FMT_INTEGER_H
+
+#include <fmt/format.h>
+
+#include <gba/core/integer.h>
+
+template<typename T> struct fmt::formatter<gba::integer<T>> : formatter<T> {
+    template <typename FormatContext>
+    auto format(gba::integer<T> i, FormatContext& ctx) {
+        return formatter<T>::format(i.get(), ctx);
+    }
+};
+
+#endif //GAMEBOIADVANCE_FMT_INTEGER_H
