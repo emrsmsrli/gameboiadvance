59c0dc12eed63070fe66c0ad176a32e6d4383a58
emrsmsrli <emrsmsrli1@gmail.com>
2020-11-21 22:23:50 +0300

    add version header generation

---
+++ gba/include/gba/version.h
@@ -0,0 +1,16 @@
+#ifndef GAMEBOIADVANCE_VERSION_H
+#define GAMEBOIADVANCE_VERSION_H
+
+#include <string_view>
+
+namespace gba {
+
+[[maybe_unused]] constexpr auto version_major = 0;
+[[maybe_unused]] constexpr auto version_minor = 0;
+[[maybe_unused]] constexpr auto version_patch = 1;
+
+[[maybe_unused]] constexpr std::string_view version = "0.0.1";
+
+} // namespace gba
+
+#endif //GAMEBOIADVANCE_VERSION_H
