023f473a123dab93a12866fd48fda4b6cf661414
emrsmsrli <emrsmsrli1@gmail.com>
2020-11-22 22:37:13 +0300

    fix log macros

--- gba/include/gba/helper/macros.h
+++ gba/include/gba/helper/macros.h
@@ -3,12 +3,12 @@
 
 #include <spdlog/spdlog.h>
 
-#define LOG_CRITICAL(fmt, ...) SPDLOG_CRITICAL(fmt, __VA_ARGS__) /*NOLINT*/
-#define LOG_ERROR(fmt, ...) SPDLOG_ERROR(fmt, __VA_ARGS__) /*NOLINT*/
-#define LOG_WARN(fmt, ...) SPDLOG_WARN(fmt, __VA_ARGS__) /*NOLINT*/
-#define LOG_INFO(fmt, ...) SPDLOG_INFO(fmt, __VA_ARGS__) /*NOLINT*/
-#define LOG_DEBUG(fmt, ...) SPDLOG_DEBUG(fmt, __VA_ARGS__) /*NOLINT*/
-#define LOG_TRACE(fmt, ...) SPDLOG_TRACE(fmt, __VA_ARGS__) /*NOLINT*/
+#define LOG_CRITICAL(...) SPDLOG_CRITICAL(__VA_ARGS__) /*NOLINT*/
+#define LOG_ERROR(...) SPDLOG_ERROR(__VA_ARGS__) /*NOLINT*/
+#define LOG_WARN(...) SPDLOG_WARN(__VA_ARGS__) /*NOLINT*/
+#define LOG_INFO(...) SPDLOG_INFO(__VA_ARGS__) /*NOLINT*/
+#define LOG_DEBUG(...) SPDLOG_DEBUG(__VA_ARGS__) /*NOLINT*/
+#define LOG_TRACE(...) SPDLOG_TRACE(__VA_ARGS__) /*NOLINT*/
 
 #if defined(_MSC_VER)
   #define FORCEINLINE __forceinline
