b4df38dff863eecb52b5792a6e3e23d72c5c5621
emrsmsrli <emrsmsrli1@gmail.com>
2020-11-28 11:09:51 +0300

    refactor gamepak

--- gba/include/gba/cartridge/gamepak.h
+++ gba/include/gba/cartridge/gamepak.h
@@ -1,7 +1,7 @@
 #ifndef GAMEBOIADVANCE_GAMEPAK_H
 #define GAMEBOIADVANCE_GAMEPAK_H
 
-#include <string>
+#include <string_view>
 
 #include <gba/core/event/event.h>
 #include <gba/helper/filesystem.h>
@@ -14,11 +14,11 @@ class gamepak {
     event<const fs::path&> on_load_;
 
     fs::path path_;
-    vector<u8> rom_data_;
+    vector<u8> pak_data_;
 
-    std::string game_title_;
-    std::string game_code_;
-    std::string maker_code_;
+    std::string_view game_title_;
+    std::string_view game_code_;
+    std::string_view maker_code_;
     u8 main_unit_code_;
     u8 software_version_;
     u8 checksum_;
@@ -31,7 +31,7 @@ public:
     [[nodiscard]] event<const fs::path&>& on_load() noexcept { return on_load_; }
     [[nodiscard]] bool loaded() const noexcept { return loaded_; }
 
-    [[nodiscard]] vector<u8>& rom_data() noexcept { return rom_data_; }
+    [[nodiscard]] vector<u8>& pak_data() noexcept { return pak_data_; }
 };
 
 } // namespace gba
--- gba/src/cartridge/gamepak.cpp
+++ gba/src/cartridge/gamepak.cpp
@@ -4,35 +4,40 @@ namespace gba {
 
 namespace {
 
-void copy_pak_str(std::string& out, const vector<u8>& data, const u8 start_addr, const u32 max_count) noexcept
+std::string_view make_pak_str(const vector<u8>& data, const usize start, const usize len) noexcept
 {
-    out.clear();
-    for(u32 i = 0u; i < max_count; ++i) {
-        const auto c = data[start_addr + i];
-        if(c == 0) { break; }
-        out.push_back(c.get());
+    return std::string_view{reinterpret_cast<const char*>(data.data() + start.get()), len.get()};  //NOLINT
+}
+
+std::string_view make_pak_str_zero_padded(const vector<u8>& data, const usize start, const usize max_len) noexcept
+{
+    usize len;
+    for(; len < max_len; ++len) {
+        if(data[start + len] == 0) { return make_pak_str(data, start, len + 1_usize); }
     }
+
+    return make_pak_str(data, start, len);
 }
 
 } // namespace
 
 void gamepak::load(const fs::path& path)
 {
-    rom_data_ = fs::read_file(path);
+    pak_data_ = fs::read_file(path);
     path_ = path;
     loaded_ = true;
 
-    copy_pak_str(game_title_, rom_data_, 0xA0_u8, 12_u32);
-    copy_pak_str(game_code_, rom_data_, 0xAC_u8, 4_u32);
-    copy_pak_str(maker_code_, rom_data_, 0xB0_u8, 2_u32);
+    game_title_ = make_pak_str_zero_padded(pak_data_, 0xA0_usize, 12_usize);
+    game_code_ = make_pak_str_zero_padded(pak_data_, 0xAC_usize, 4_usize);
+    maker_code_ = make_pak_str_zero_padded(pak_data_, 0xB0_usize, 2_usize);
 
-    main_unit_code_ = rom_data_[0xB3_u32];
-    software_version_ = rom_data_[0xBC_u32];
-    checksum_ = rom_data_[0xBD_u32];
+    main_unit_code_ = pak_data_[0xB3_u32];
+    software_version_ = pak_data_[0xBC_u32];
+    checksum_ = pak_data_[0xBD_u32];
 
     u8 calculated_checksum;
     for(u32 addr = 0xA0_u32; addr < 0xBD_u32; ++addr) {
-        calculated_checksum -= rom_data_[addr];
+        calculated_checksum -= pak_data_[addr];
     }
     calculated_checksum -= 0x19_u8;
 
@@ -45,7 +50,7 @@ void gamepak::load(const fs::path& path)
     LOG_INFO("software version: {}", software_version_);
 
     if(calculated_checksum != checksum_) {
-        LOG_WARN("checksum: {:02X} - incorrect, found: {:02X}", calculated_checksum, checksum_);
+        LOG_WARN("checksum: {:02X} - mismatch, found: {:02X}", calculated_checksum, checksum_);
     } else {
         LOG_INFO("checksum: {:02X}", calculated_checksum);
     }
