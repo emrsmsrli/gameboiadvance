7cefcbafa52931fd51d5eb242b00beb49da70a8c
emrsmsrli <emrsmsrli1@gmail.com>
2021-07-22 21:14:59 +0300

    implement windows version of mmap

--- gba/include/gba/helper/filesystem.h
+++ gba/include/gba/helper/filesystem.h
@@ -9,6 +9,8 @@
 #define GAMEBOIADVANCE_FILESYSTEM_H
 
 #include <filesystem>
+#include <memory>
+#include <system_error>
 
 #include <gba/core/container.h>
 
@@ -18,6 +20,68 @@ using namespace std::filesystem;
 
 vector<u8> read_file(const path& path);
 void write_file(const path& path, const vector<u8>& data);
+
+class mmap {
+    fs::path path_;
+    usize mapped_size_;
+
+    struct impl;
+    std::unique_ptr<impl> impl_ = nullptr;
+
+public:
+    static inline constexpr auto map_whole_file = 0_usize;
+
+    using value_type = u8;
+    using size_type = usize;
+    using reference = u8&;
+    using const_reference = const u8&;
+    using pointer = u8*;
+    using const_pointer = const u8*;
+    using iterator = pointer;
+    using const_iterator = const_pointer;
+
+    mmap() noexcept;
+    mmap(fs::path path, std::error_code& err) noexcept;
+    mmap(fs::path path, usize map_size, std::error_code& err) noexcept;
+
+    ~mmap() noexcept;
+
+    // move only
+    mmap(const mmap&) = delete;
+    mmap(mmap&&) noexcept;
+
+    mmap& operator=(const mmap&) = delete;
+    mmap& operator=(mmap&&) noexcept;
+
+    [[nodiscard]] u8& operator[](const usize idx) noexcept { return *ptr(idx); }
+    [[nodiscard]] u8 operator[](const usize idx) const noexcept { return *ptr(idx); }
+    [[nodiscard]] u8* ptr(usize idx) noexcept;
+    [[nodiscard]] const u8* ptr(usize idx) const noexcept;
+    [[nodiscard]] u8& at(const usize idx) noexcept { ASSERT(idx < mapped_size_); return *ptr(idx); }
+    [[nodiscard]] u8 at(const usize idx) const noexcept { ASSERT(idx < mapped_size_); return *ptr(idx); }
+    [[nodiscard]] u8* data() noexcept { return ptr(0_usize); }
+    [[nodiscard]] const u8* data() const noexcept { return ptr(0_usize); }
+
+    [[nodiscard]] constexpr usize size() const noexcept { return mapped_size_; }
+
+    [[nodiscard]] u8& front() noexcept { return at(0_usize); }
+    [[nodiscard]] u8 front() const noexcept { return at(0_usize); }
+    [[nodiscard]] u8& back() noexcept { return at(size() - 1_usize); }
+    [[nodiscard]] u8 back() const noexcept { return at(size() - 1_usize); }
+
+    void map(std::error_code& err) noexcept;
+    void map(usize map_size, std::error_code& err) noexcept;
+    void unmap() noexcept;
+    void flush() const noexcept;
+    [[nodiscard]] bool is_mapped() const noexcept;
+
+    [[nodiscard]] iterator begin() noexcept { return data(); }
+    [[nodiscard]] iterator end() noexcept { return ptr(mapped_size_); }
+    [[nodiscard]] const_iterator begin() const noexcept { return data(); }
+    [[nodiscard]] const_iterator end() const noexcept { return ptr(mapped_size_); }
+    [[nodiscard]] const_iterator cbegin() const noexcept { return data(); }
+    [[nodiscard]] const_iterator cend() const noexcept { return ptr(mapped_size_); }
+};
 
 } // namespace gba::fs
 
--- gba/src/helper/filesystem.cpp
+++ gba/src/helper/filesystem.cpp
@@ -8,6 +8,11 @@
 #include <gba/helper/filesystem.h>
 
 #include <fstream>
+#include <memory>
+#include <system_error>
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
 
 namespace gba::fs {
 
@@ -39,6 +44,152 @@ void write_file(const path& path, const vector<u8>& data)
 
     stream.write(reinterpret_cast<const char*>(data.data()), data.size().get()); // NOLINT
     LOG_TRACE(fs, "wrote {} bytes to {}", data.size(), path.string());
+}
+
+struct mmap::impl {
+    using handle_type = HANDLE;
+    static inline const handle_type invalid_handle = INVALID_HANDLE_VALUE; // NOLINT
+
+    HANDLE file_handle = invalid_handle;
+    HANDLE file_mapping_handle = invalid_handle;
+    u8* map_ptr = nullptr;
+
+    void map(const path& path, usize map_size, std::error_code& err) noexcept;
+    void unmap() noexcept;
+    void flush(usize flush_size) const noexcept;
+    [[nodiscard]] bool is_mapped() const noexcept { return file_mapping_handle != invalid_handle; }
+    [[nodiscard]] static int get_last_error() noexcept;
+};
+
+mmap::mmap() noexcept {}
+
+mmap::mmap(std::filesystem::path path, std::error_code& err) noexcept
+  : mmap(std::move(path), map_whole_file, err) {}
+
+mmap::mmap(path path, const usize map_size, std::error_code& err) noexcept
+  : path_{std::move(path)},
+    mapped_size_{map_size},
+    impl_{std::make_unique<impl>()}
+{
+    map(map_size, err);
+}
+
+mmap::~mmap() noexcept
+{
+    unmap();
+}
+
+const u8* mmap::ptr(const usize idx) const noexcept
+{
+    ASSERT(impl_ != nullptr);
+    return impl_->map_ptr + idx.get(); // NOLINT
+}
+
+u8* mmap::ptr(const usize idx) noexcept
+{
+    ASSERT(impl_ != nullptr);
+    return impl_->map_ptr + idx.get(); // NOLINT
+}
+
+mmap::mmap(mmap&& other) noexcept
+  : path_{std::move(other.path_)},
+    mapped_size_{std::exchange(other.mapped_size_, 0_usize)},
+    impl_{std::move(other.impl_)} {}
+
+mmap& mmap::operator=(mmap&& other) noexcept
+{
+    path_ = std::move(other.path_);
+    mapped_size_ = std::exchange(other.mapped_size_, 0_usize);
+    impl_ = std::move(other.impl_);
+    return *this;
+}
+
+void mmap::map(std::error_code& err) noexcept
+{
+    map(map_whole_file, err);
+}
+
+void mmap::map(const usize map_size, std::error_code& err) noexcept
+{
+    if(!impl_) {
+        impl_ = std::make_unique<impl>();
+    }
+
+    mapped_size_ = map_size;
+    impl_->map(path_, map_size, err);
+
+    if(is_mapped() && map_size == map_whole_file) {
+        mapped_size_ = fs::file_size(path_);
+    }
+}
+
+void mmap::unmap() noexcept
+{
+    if(impl_) {
+        impl_->unmap();
+        mapped_size_ = 0_usize;
+    }
+}
+
+void mmap::flush() const noexcept
+{
+    if(impl_) {
+        impl_->flush(mapped_size_);
+    }
+}
+
+bool mmap::is_mapped() const noexcept
+{
+    if(impl_) {
+        return impl_->is_mapped();
+    }
+    return false;
+}
+
+void mmap::impl::map(const path& path, usize map_size, std::error_code& err) noexcept
+{
+    file_handle = CreateFileW(path.c_str(),
+      GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, nullptr,
+      OPEN_EXISTING, FILE_FLAG_RANDOM_ACCESS, nullptr);
+    if(file_handle == invalid_handle) {
+        err.assign(impl::get_last_error(), std::system_category());
+        LOG_ERROR(fs, "could not open file handle: {}, {}", err.message(), path.string());
+        std::terminate();
+    }
+
+    file_mapping_handle = CreateFileMappingW(file_handle,
+      nullptr, PAGE_READWRITE,
+      map_size.get() >> 32, map_size.get(), nullptr);
+    if(file_mapping_handle == invalid_handle) {
+        err.assign(impl::get_last_error(), std::system_category());
+        LOG_ERROR(fs, "could not create file mapping: {}, {}", err.message(), path.string());
+        std::terminate();
+    }
+
+    map_ptr = static_cast<u8*>(MapViewOfFile(file_mapping_handle,
+      FILE_MAP_ALL_ACCESS, 0, 0, map_size.get()));
+}
+
+void mmap::impl::unmap() noexcept
+{
+    if(is_mapped()) {
+        UnmapViewOfFile(map_ptr);
+        CloseHandle(file_mapping_handle);
+        CloseHandle(file_handle);
+        map_ptr = nullptr;
+        file_mapping_handle = invalid_handle;
+        file_handle = invalid_handle;
+    }
+}
+
+void mmap::impl::flush(const usize flush_size) const noexcept
+{
+    FlushViewOfFile(map_ptr, flush_size.get());
+}
+
+int mmap::impl::get_last_error() noexcept
+{
+    return static_cast<int>(GetLastError());
 }
 
 } // namespace gba::fs
