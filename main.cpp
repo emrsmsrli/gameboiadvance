c4240e8468bcc47379c0ec7d6cc559555ff0ce6e
emrsmsrli <emrsmsrli1@gmail.com>
2021-07-23 20:16:52 +0300

    implement spdlog file sink

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include <cxxopts.hpp>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/daily_file_sink.h>
 #include <spdlog/spdlog.h>
 #include <sdl2cpp/sdl_core.h>
 
@@ -17,8 +18,10 @@
 int main(int argc, char** argv)
 {
 #if SPDLOG_ACTIVE_LEVEL != SPDLOG_LEVEL_OFF
+    static constexpr const char* log_pattern = "[%H:%M:%S:%e] [%s:%#] [%^%L%$] %v";
+
     spdlog::set_level(spdlog::level::trace);
-    spdlog::set_pattern("[%H:%M:%S:%e] [%s:%#] [%^%L%$] %v");
+    spdlog::set_pattern(log_pattern);
     spdlog::set_default_logger(spdlog::stdout_color_st("core"));
 #endif // SPDLOG_ACTIVE_LEVEL != SPDLOG_LEVEL_OFF
 
@@ -29,6 +32,9 @@ int main(int argc, char** argv)
       .add_options()
         ("v,version", "Print version and exit")
         ("h,help", "Show this help text")
+#if SPDLOG_ACTIVE_LEVEL != SPDLOG_LEVEL_OFF
+        ("enable-file-log", "Enables file logging", cxxopts::value<bool>()->default_value("true"))
+#endif // SPDLOG_ACTIVE_LEVEL != SPDLOG_LEVEL_OFF
         ("fullscreen", "Enable fullscreen")
         ("S,viewport-scale", "Scale of the viewport (not used if fullscreen is set), (240x160)*S", cxxopts::value<uint32_t>()->default_value("2"))
         ("bios", "BIOS binary path", cxxopts::value<std::string>())
@@ -37,6 +43,14 @@ int main(int argc, char** argv)
     options.parse_positional("rom-path");
 
     const auto parsed = options.parse(argc, argv);
+
+#if SPDLOG_ACTIVE_LEVEL != SPDLOG_LEVEL_OFF
+    if(parsed["enable-file-log"].as<bool>()) {
+        auto file_log_sink = std::make_shared<spdlog::sinks::daily_file_sink_st>("logs/gba.log", 0, 0, false, 5);
+        file_log_sink->set_pattern(log_pattern);
+        spdlog::default_logger()->sinks().push_back(std::move(file_log_sink));
+    }
+#endif // SPDLOG_ACTIVE_LEVEL != SPDLOG_LEVEL_OFF
 
     if(parsed["version"].as<bool>()) {
         fmt::print(stdout, "gameboiadvance v{}", gba::version);
