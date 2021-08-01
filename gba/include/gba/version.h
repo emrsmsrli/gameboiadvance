415d94bc41bf3710f69edc878acd980e5edc36ed
emrsmsrli <emrsmsrli1@gmail.com>
2021-08-02 00:14:43 +0300

    bump version

--- gba/include/gba/version.h
+++ gba/include/gba/version.h
@@ -14,9 +14,9 @@ namespace gba {
 
 [[maybe_unused]] constexpr auto version_major = 0;
 [[maybe_unused]] constexpr auto version_minor = 8;
-[[maybe_unused]] constexpr auto version_patch = 7;
+[[maybe_unused]] constexpr auto version_patch = 9;
 
-[[maybe_unused]] constexpr std::string_view version = "0.8.7";
+[[maybe_unused]] constexpr std::string_view version = "0.8.9";
 
 } // namespace gba
 
